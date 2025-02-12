4664c61dbee8edf376f7c21a4b219388748bd20c
mahanandhan <mahanandhanmanish@gmail.com>
2025-02-12 09:03:58 +0530

    deployed

---
+++ frontend/src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <div>
+      <h1>Person Info</h1>
+      <p>Name: mahanandhan</p>
+      <p>contact details: 999999999</p>
+    </div>
+  )
+}
+
+export default Home
